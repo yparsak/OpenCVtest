a18aea8000232a54d83ab88fd3a63eee0968f48c
Y. E. Parsak <parsak@gmail.com>
2017-08-28 16:34:25 -0400

    Update mainwindow.cpp

--- opencvtest/mainwindow.cpp
+++ opencvtest/mainwindow.cpp
@@ -1,9 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
